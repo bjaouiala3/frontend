605e86e21a143d85c6863c3c79b3bbeccaedb2a5
bjaouiala3 <alabjaoui523@gmail.com>
2026-02-01 22:36:21 +0100

    workflow change

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Task, CreateTaskDto, UpdateTaskDto } from '../types/task';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
 
 const api = axios.create({
   baseURL: API_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+  },
+  server: {
+    host: true, // allow connections from external hosts
+    allowedHosts: ['taskmanager.com'], // whitelist your fake domain
+    port: 5173,
   },
 })
